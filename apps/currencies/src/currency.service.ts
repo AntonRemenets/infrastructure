90a1efcfa628f7a23050459406430bf984b906d9
anton-ubuntu <aremenets1@gmail.com>
2024-07-19 17:55:28 +0500

    some fixes

--- apps/currencies/src/currency.service.ts
+++ apps/currencies/src/currency.service.ts
@@ -1,21 +1,18 @@
 import { Injectable } from '@nestjs/common'
 import { HttpService } from '@nestjs/axios'
-import * as process from 'node:process'
 import { firstValueFrom } from 'rxjs'
-import { CurrencyResponse } from '../../../shared/contracts/currency.response'
 import { CurrencyModel } from './models/currency.model'
+import { CurrencyResponse } from '../../../shared/contracts/currency.response'
 
 @Injectable()
 export class CurrencyService {
   constructor(private readonly httpService: HttpService) {}
+  private URL =
+    process.env.CURRENCY_API_URI + process.env.CURRENCY_API_KEY + '/latest/USD'
 
   async get(): Promise<Promise<CurrencyResponse> | void> {
-    const URL: string =
-      process.env.CURRENCY_API_URI +
-      process.env.CURRENCY_API_KEY +
-      '/latest/USD'
     const { data } = await firstValueFrom(
-      this.httpService.get<CurrencyModel>(URL),
+      this.httpService.get<CurrencyModel>(this.URL),
     )
 
     return {
